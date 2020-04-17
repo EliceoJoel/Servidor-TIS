d4ef9c26262b0cdcff0a8ec5e322b9994cf7c82f
marcos27SG <marcosrsg27@gmail.com>
2020-04-17 14:05:32 -0400

    controlador funcionando

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -8,6 +8,7 @@ use App\Announcement;
 class AnnouncementController extends Controller
 {
     public function add(Request $request){
+<<<<<<< HEAD
         $filename = $request->file('filepdf') -> getClientOriginalName();
         $announcement = $request->file('filepdf')->storeAs('filepdf', $filename);
         $announcement = Announcement::create($request->all());
@@ -21,6 +22,22 @@ class AnnouncementController extends Controller
 
     public function get($id){
         $announcement = Announcement::find($id);
+=======
+        //$announcement = Announcement::create($request->input('name'));
+        //$announcement = Announcement::create($request->input('year'));
+        //$announcement = Announcement::create($request->input('type'));
+        //$announcement = Announcement::create($request->input('departament'));
+        //$announcement = Announcement::create($request->input('auxiliary'));
+        //$announcement = Announcement::create($request->input('file'));
+        $announcement = $request->file('file')->store('file');
+        $announcement = Announcement::create($request->all());
+       
+        // if($request -> has('file')){
+
+        //      $announcement ->add(['file' => $request ->file('file') -> store ('announcementpost   ','public')]);  
+         
+        // } 
+>>>>>>> controlador funcionando
         return $announcement;
     }
 }
