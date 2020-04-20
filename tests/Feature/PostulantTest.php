7277f26200eb9bd81f6ab8e71c0cb8ca7de31cb8
JosMaria <josmariaguilar@gmail.com>
2020-04-20 17:48:52 -0600

    test post agregado de datos

--- tests/Feature/PostulantTest.php
+++ tests/Feature/PostulantTest.php
@@ -7,8 +7,8 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class PostulantTest extends TestCase
-{
+class PostulantTest extends TestCase{
+
     /**
      * A basic test example.
      *
@@ -21,4 +21,20 @@ class PostulantTest extends TestCase
                                         'direction','email','phone','ci','auxiliary']);
     }
 
+    public function testPostPostulant(){
+        $response = $this->json('POST','api/postulant',[
+                'id' => 1223,
+                'names' => 'marcela',
+                'first_surname' => 'paco',
+                'second_surname' => 'terrazas',
+                'direction' => 'lejos',
+                'email' => 'machy@gmail.com',
+                'phone' => 2133432,
+                'ci' => 2347673,
+                'auxiliary' => 'aun_todavia']);
+
+        $response->assertJsonStructure(['id','names','first_surname','second_surname',
+                                'direction','email','phone','ci','auxiliary']);
+        $response->status(201);
+    }
 }
