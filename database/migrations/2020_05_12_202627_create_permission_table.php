3e64f165badac067dbd9d46f3ffbd4e9825b4b1e
Elizeo <eliceoherbas@gmail.com>
2020-05-13 19:08:38 -0400

    se anadio columna route a persmission

--- database/migrations/2020_05_12_202627_create_permission_table.php
+++ database/migrations/2020_05_12_202627_create_permission_table.php
@@ -17,6 +17,7 @@ class CreatePermissionTable extends Migration
             $table->increments('id');
             $table->string('permission');
             $table->integer('idRol');
+            $table->string('route');
             $table->timestamps();
         });
     }
