306ad53ca49305381f6bf37c64e2c901be25b0cd
Mau123123 <Mau123123@users.noreply.github.com>
2020-05-16 17:34:36 -0400

    update user table and user_announcement table

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Http\Request;
 Route::post('register', 'UserController@register');
 Route::post('login', 'UserController@login');
 Route::get('profile', 'UserController@getAuthenticatedUser');
+Route::post('userAnnouncement','UserAnnouncementController@saveAnnouncement')->name('addAnnouncementToUser');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
