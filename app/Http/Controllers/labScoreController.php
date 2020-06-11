0a36c0a4b5abf18ccbc6db5d0ef447ec563f0cfc
Mau123123 <Mau123123@users.noreply.github.com>
2020-06-11 16:04:37 -0400

    asd

--- app/Http/Controllers/labScoreController.php
+++ app/Http/Controllers/labScoreController.php
@@ -8,7 +8,19 @@ use Illuminate\Http\Request;
 class labScoreController extends Controller
 {
     public function add(Request $request){
-        $postulantScore = Laboratory_score::create($request->all());
-        return $postulantScore;
+        $idPostulant = $request ->get('idPostulant');
+        $idtTheme = $request ->get('idtTheme');
+        $score = $request ->get('score');
+        if (Laboratory_score::where('idPostulant', '=', $idPostulant)->exists()){
+            DB::update('
+                update laboratory_socres
+                set score= ?
+                where "idPostulant" = ? and "idtTheme" = ?'
+            ,[$score,$idPostulant,$idtTheme]);
+        }
+        else{
+            $postulantScore = Laboratory_score::create($request->all());
+            return $postulantScore;
+        }
     }
 }
